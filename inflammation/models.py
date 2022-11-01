34c3b4c872d6b2b194a89903e139a0c4cb86a63a
Mamtsits <elizabeth.mamtsits@stfc.ac.uk>
2022-11-01 12:32:09 +0000

    style changes to doc-strings

--- inflammation/models.py
+++ inflammation/models.py
@@ -11,9 +11,11 @@ import numpy as np
 
 
 def load_csv(filename):
+
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
+    
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
