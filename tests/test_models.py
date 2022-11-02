dec06dc6768734f3bdc7d2195a20df7fe73aada8
Mamtsits <elizabeth.mamtsits@stfc.ac.uk>
2022-11-02 11:45:41 +0000

    major changes to tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,18 +5,27 @@ import numpy.testing as npt
 import pytest
 
 @pytest.mark.parametrize(
-    "test, expected", [
-([[0, 0],[0, 0],[0, 0]], [0, 0]),
-([[1, 2],[3, 4],[5, 6]], [5, 6])]
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ]
 )
-def test_daily_mean_zeros(test, expexted):
+def test_daily_mean(test, expected):
     """Test that mean function works for an array of zeros."""
     from inflammation.models import daily_mean
 
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test), expexted)
+    npt.assert_array_equal(daily_mean(test), expected)
 
-@@pytest.mark.parametrize(
+def test_daily_min_string():
+    '''Test for TypeError when we pass a string'''
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['abd', 'ads'], ['asd', 'auhs']])
+
+@pytest.mark.parametrize(
     "test, expected",
     [
         ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
@@ -42,16 +51,3 @@ def test_daily_min(test, expected):
     from inflammation.models import daily_min
 
     npt.assert_array_equal(daily_min(test), expected)
-
-def test_daily_min_string():
-    """Test that mean function works for an array of zeros."""
-    from inflammation.models import daily_min
-
-    with pytest.raises(TypeError):
-        error_expected = daily_min(['abd', 'ads'], ['aff','gfy'])
-
-
-
-
-
-
